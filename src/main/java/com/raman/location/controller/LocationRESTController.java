1c5c53101ebd12cbeeee33280b90402fb511f235
Krishan Pandita Raman (Supplier) <raman.krishan.pandita@supplier.volvo.com>
2019-01-28 15:37:19 +0530

    Added REST controller

---
+++ src/main/java/com/raman/location/controller/LocationRESTController.java
@@ -0,0 +1,55 @@
+package com.raman.location.controller;
+
+import java.util.List;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.raman.location.entities.Location;
+import com.raman.location.repositories.LocationRepository;
+
+@RestController
+@RequestMapping("/locations")
+public class LocationRESTController {
+	
+	@Autowired
+	LocationRepository locationRepository;
+	
+	@GetMapping
+	public List<Location> displayLocations() {
+		return locationRepository.findAll();
+	}
+	
+	@PostMapping
+	public Location createLocation(@RequestBody Location location) {
+		return locationRepository.save(location);
+	}
+
+	@PutMapping
+	public Location updateLocation(@RequestBody Location location) {
+		return locationRepository.save(location);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteLocation(@PathVariable int id) {
+		Location location = new Location();
+		location.setId(id);
+		locationRepository.delete(location);
+	}
+	
+	@GetMapping("/{id}")
+	public Location getLocation(@PathVariable int id) {
+		return locationRepository.findById(id).orElseThrow(()->new EntityNotFoundException());
+	}
+	
+}
