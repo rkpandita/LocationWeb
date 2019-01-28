d3f7d02efe8d51c0ca5dbfd9c64ce40c5a45b3f9
Krishan Pandita Raman (Supplier) <raman.krishan.pandita@supplier.volvo.com>
2019-01-28 14:39:50 +0530

    Added Email functionality

--- src/main/java/com/raman/location/controller/LocationController.java
+++ src/main/java/com/raman/location/controller/LocationController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.servlet.ServletContext;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -13,16 +15,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.raman.location.entities.Location;
 import com.raman.location.services.LocationService;
+import com.raman.location.util.EmailUtil;
 import com.raman.location.util.ReportUtil;
 
 @Controller
 public class LocationController {
+	
+	private Log logger = LogFactory.getLog(LocationController.class);
 
 	@Autowired
 	private LocationService service;
 
 	@Autowired
 	private ReportUtil reportUtil;
+	
+	@Autowired
+	private EmailUtil emailUtil;
 	
 	@Autowired
 	private ServletContext context;
@@ -38,6 +46,10 @@ public class LocationController {
 		Location locationSaved = service.saveLocation(location);
 		String msg = "Location saved with name: " + locationSaved.getName();
 		modelMap.addAttribute("msg", msg);
+		
+		logger.info("Email functionality starts");
+		emailUtil.sendEmail("springemail.config@gmail.com", "Location Saved", "Location Saved mail sent");
+		
 		return "createLocation";
 	}
 
@@ -81,6 +93,5 @@ public class LocationController {
 		reportUtil.generatePieChart(path, data);
 		return "report";
 	}
-	
 	
 }
